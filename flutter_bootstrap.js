b880ad114769725c95b65e0e26dd09964f7ade1d
Ron Edgar <ron.edg@gmail.com>
2026-03-24 20:51:23 -0400

    publish web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "927137636"
+    serviceWorkerVersion: "458909237"
   }
 });
