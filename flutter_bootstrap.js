7dffffbad0a7055189287b767aeb488f683e5a85
Ron Edgar <ron.edg@gmail.com>
2026-03-30 12:08:59 -0400

    publish web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2648582681"
+    serviceWorkerVersion: "1346322572"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bc769a38997511522e260c6785a156de",
+const RESOURCES = {"flutter_bootstrap.js": "e409203edadfb7be2fc3324897729cc1",
 "version.json": "b1de9b838eccc4df43e7d4f500d1891d",
 "index.html": "50acbd3e25ecf85606b7d08984d32139",
 "/": "50acbd3e25ecf85606b7d08984d32139",
-"main.dart.js": "00a70758c02f034b067dcd4ad9769140",
+"main.dart.js": "ddfc194d58dc00835f14bcdb6539165f",
 "sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "3d7923ab760d48cd8c837c1aef9ba703",
@@ -25,7 +25,7 @@ const RESOURCES = {"flutter_bootstrap.js": "bc769a38997511522e260c6785a156de",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "e0402611f148f988bcdab7dc3a177bda",
-"assets/fonts/MaterialIcons-Regular.otf": "a6f3c17df256285838eba44c8497a0b7",
+"assets/fonts/MaterialIcons-Regular.otf": "c577d9740ae0330a0c4a41111aa57a06",
 "assets/assets/vehicles/suv.svg": "58f0fdbdb67b2691b787bd9f95e36406",
 "assets/assets/vehicles/jeep.svg": "d135efb49d82bbc33cdea38a3dd4a388",
 "assets/assets/vehicles/pickup.svg": "4de057eecf384fb99aac9a44c2c0546f",
